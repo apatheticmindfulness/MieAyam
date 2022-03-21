0626deccbfa2098f101843eb6a174e1996e2d1f0
apatheticmindfulness <rizkymk11@gmail.com>
2022-03-21 22:33:39 +0800

    Menambahkan fungsi untuk mouse input

--- src/may_window.c
+++ src/may_window.c
@@ -1,12 +1,34 @@
 #include <stdlib.h>
 #include "may_window.h"
 
+struct MAY_Mouse
+{
+	int8_t		isValid;
+	int32_t		x;
+	int32_t		y;
+	MAY_MOUSE_STATE state;
+};
+
+struct MAY_Window
+{
+	int32_t		width;
+	int32_t		height;
+	int8_t		isValid;
+	void		*pHandle;
+	const char	*pTitle;
+
+	MAY_Mouse mouse;
+};
+
 // Variables
 static WNDCLASSEX _MAY_WindowClass;
 
 // Functions
 static LRESULT CALLBACK _MAY_Win32HandleMessageState(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 static LRESULT CALLBACK _MAY_Win32HandleProcess(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+static inline MAY_Window * _MAY_GetWindowState(HWND hwnd);
+static void _MAY_SendMouseLeftClickedState(MAY_Window * window, int32_t x, int32_t y);
+static void _MAY_SendMouseLeftReleased(MAY_Window * window, int32_t x, int32_t y);
 
 uint8_t MAY_Init()
 {
@@ -26,9 +48,9 @@ uint8_t MAY_Init()
 	return true;
 }
 
-uint8_t MAY_ShowWindow(MAY_Window window)
+uint8_t MAY_ShowWindow(MAY_Window * window)
 {
-	if (ShowWindow(window.pHandle, SW_SHOW))
+	if (ShowWindow(window->pHandle, SW_SHOW))
 	{
 		return true;
 	}
@@ -57,50 +79,111 @@ uint8_t MAY_ProcessMessage()
 	return true;
 }
 
-MAY_Window MAY_CreateWindow(uint32_t width, uint32_t height, const char * title)
+uint8_t MAY_MouseLeftIsPressed(MAY_Mouse * mouse)
 {
-	MAY_Window window =
+	return mouse->state == MAY_MOUSE_CLICKED;
+}
+
+uint8_t MAY_MouseLeftIsReleased(MAY_Mouse * mouse)
+{
+	if (mouse->state == MAY_MOUSE_RELEASED)
 	{
-		.width = width,
-		.height = height,
-		.pTitle = title,
-		.isValid = true
-	};
+		return true;
+	}
+	return false;
+}
 
-	// Create window handle and adjusting it
-	RECT rc = { 0, 0, (LONG)window.width, (LONG)window.height };
-	AdjustWindowRect(&rc, WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX, false);
-
-	HWND windowHandle = CreateWindow(
-		_MAY_WindowClass.lpszClassName,
-		window.pTitle,
-		WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		rc.right - rc.left,
-		rc.bottom - rc.top,
-		0,
-		0,
-		GetModuleHandle(0),
-		&window);
-
-	window.pHandle = windowHandle;
-
-	if (!window.pHandle)
+MAY_Window * MAY_CreateWindow(uint32_t width, uint32_t height, const char * title)
+{
+	MAY_Window * window = (MAY_Window*)malloc(sizeof(MAY_Window));
+	if (window)
 	{
-		window.isValid = false;
+		// Zero all for window
+		memset(window, 0, sizeof(MAY_Window));
+		window->width = width;
+		window->height = height;
+		window->pTitle = title;
+
+		// Create window handle and adjusting it
+		RECT rc = { 0, 0, (LONG)window->width, (LONG)window->height };
+		AdjustWindowRect(&rc, WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX, false);
+
+		HWND windowHandle = CreateWindow(
+			_MAY_WindowClass.lpszClassName,
+			window->pTitle,
+			WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			rc.right - rc.left,
+			rc.bottom - rc.top,
+			0,
+			0,
+			GetModuleHandle(0),
+			window);
+
+		window->pHandle = windowHandle;
+
+		if (!window->pHandle)
+		{
+			window->isValid = false;
+		}
+		return window;
 	}
 
-	return window;
+	return 0;
+}
+
+void MAY_FreeWindow(MAY_Window * window)
+{
+	if (window)
+	{
+		free(window);
+		window = 0;
+	}
+}
+
+void * MAY_GetWindowHandle(MAY_Window * window)
+{
+	if (window)
+	{
+		return window->pHandle;
+	}
+	return 0;
+}
+
+void * MAY_GetWindowMouseHandle(MAY_Window * window)
+{
+	if (window)
+	{
+		return &window->mouse;
+	}
+	return 0;
+}
+
+int32_t MAY_GetMouseX(const MAY_Window * const window)
+{
+	if (window)
+	{
+		return window->mouse.x;
+	}
+	return 0;
+}
+
+int32_t MAY_GetMouseY(const MAY_Window * const window)
+{
+	if (window)
+	{
+		return window->mouse.x;
+	}
+	return 0;
 }
 
 LRESULT _MAY_Win32HandleMessageState(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	if (uMsg == WM_CREATE)
+	if (uMsg == WM_NCCREATE)
 	{
 		CREATESTRUCT *pCreate = (CREATESTRUCT*)(lParam);
 		MAY_Window * pWindowState = (MAY_Window*)(pCreate->lpCreateParams);
-
 		SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)(pWindowState));
 		SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)(&_MAY_Win32HandleProcess));
 		_MAY_Win32HandleProcess(hwnd, uMsg, wParam, lParam);
@@ -111,6 +194,8 @@ LRESULT _MAY_Win32HandleMessageState(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
 
 LRESULT _MAY_Win32HandleProcess(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	LONG_PTR pWindowData = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	MAY_Window * pWindowState = _MAY_GetWindowState(hwnd);
 	PAINTSTRUCT ps;
 	HDC hdc;
 
@@ -127,8 +212,47 @@ LRESULT _MAY_Win32HandleProcess(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 			hdc = BeginPaint(hwnd, &ps);
 			EndPaint(hwnd, &ps);
 		}
+		break;
+
+		// Mouse input
+		case WM_LBUTTONDOWN:
+		{
+			const POINTS point = MAKEPOINTS(lParam);
+			_MAY_SendMouseLeftClickedState(pWindowState, point.x, point.y);
+		}
+		break;
+
+		case WM_LBUTTONUP:
+		{
+			const POINTS point = MAKEPOINTS(lParam);
+			_MAY_SendMouseLeftReleased(pWindowState, point.x, point.y);
+		}
+		break;
 
 	}
 
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
+
+inline MAY_Window * _MAY_GetWindowState(HWND hwnd)
+{
+	LONG_PTR pWindowData = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	MAY_Window * pWindow = (MAY_Window*)pWindowData;
+	return pWindow;
+}
+
+void _MAY_SendMouseLeftClickedState(MAY_Window * window, int32_t x, int32_t y)
+{
+	window->mouse.state = MAY_MOUSE_CLICKED;
+	window->mouse.x = x;
+	window->mouse.y = y;
+}
+
+void _MAY_SendMouseLeftReleased(MAY_Window * window, int32_t x, int32_t y)
+{
+	window->mouse.state = MAY_MOUSE_RELEASED;
+	window->mouse.x = x;
+	window->mouse.y = y;
+}
+
+
--- src/may_window.h
+++ src/may_window.h
@@ -3,26 +3,31 @@
 #include <stdbool.h>
 #include "may_win.h"
 
-typedef struct MAY_Window
+typedef enum MAY_MOUSE_STATE
 {
-	int32_t		width;
-	int32_t		height;
-	int8_t		isValid;
-	void *		pHandle;
-	const char * pTitle;
-} MAY_Window;
+	MAY_MOUSE_NONE,
+	MAY_MOUSE_CLICKED,
+	MAY_MOUSE_RELEASED
+} MAY_MOUSE_STATE;
+typedef struct MAY_Mouse MAY_Mouse;
+typedef struct MAY_Window MAY_Window;
 
-// Functions
 uint8_t			MAY_Init();
-uint8_t			MAY_ShowWindow(MAY_Window window);
+uint8_t			MAY_ShowWindow(MAY_Window * window);
 uint8_t			MAY_ProcessMessage();
-MAY_Window		MAY_CreateWindow(uint32_t width, uint32_t height, const char * title);
+uint8_t			MAY_MouseLeftIsPressed(MAY_Mouse * mouse);
+uint8_t			MAY_MouseLeftIsReleased(MAY_Mouse * mouse);
+MAY_Window		* MAY_CreateWindow(uint32_t width, uint32_t height, const char * title);
+void			MAY_FreeWindow(MAY_Window * window);
+void			* MAY_GetWindowHandle(MAY_Window * window);
+void			* MAY_GetWindowMouseHandle(MAY_Window * window);
+
+int32_t			MAY_GetMouseX(const MAY_Window * const window);
+int32_t			MAY_GetMouseY(const MAY_Window * const window);
 
 //HWND MA_GetWindowHandle(const MA_Window * const window);
 //constexpr uint32_t MA_GetWindowWidth(const MA_Window * const window);
 //constexpr uint32_t MA_GetWindowHeight(const MA_Window * const window);
 //
-//int32_t MA_GetMouseX(const MA_Window * const window);
-//int32_t MA_GetMouseY(const MA_Window * const window);
 //uint8_t MA_KeyIsPressed(const MA_Window * const window, uint32_t keyCode);
 //uint8_t MA_KeyIsReleased(const MA_Window * const window, uint32_t keyCode);
