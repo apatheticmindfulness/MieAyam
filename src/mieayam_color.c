5503380aa422e24d08c3f71a2fbaeec31b4c795e
apatheticmindfulness <rizkymk11@gmail.com>
2022-05-22 19:53:16 +0800

    Menambahkan const pada variabel di color.c

--- src/mieayam_color.c
+++ src/mieayam_color.c
@@ -2,10 +2,10 @@
 
 mieayam_color MieAyam_MakeColorFromHex(uint32_t colorCode)
 {
-	uint8_t red = (colorCode & 0xFF0000) >> 16;
-	uint8_t green = (colorCode & 0x00FF00) >> 8;
-	uint8_t blue = (colorCode & 0x0000FF);
+	const uint8_t red = (colorCode & 0xFF0000) >> 16;
+	const uint8_t green = (colorCode & 0x00FF00) >> 8;
+	const uint8_t blue = (colorCode & 0x0000FF);
 
-	mieayam_color color = { blue, green, red, 0xFF };
+	const mieayam_color color = { blue, green, red, 0xFF };
 	return color;
 }
